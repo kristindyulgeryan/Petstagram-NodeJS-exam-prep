f5575634fa204458743313e7ad473e7f47e96bbc
Kristin <kristindyulgeryan@abv.bg>
2025-02-19 16:17:50 +0200

    Add Photo and User models validation

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -4,22 +4,30 @@ const photoShema = new Schema({
   name: {
     type: String,
     required: [true, "Name is required"],
+    minLength: [2, "Name must be at least 3 characters"],
   },
   image: {
     type: String,
     required: [true, "Image is required"],
+    match: /^https?:\/\//,
   },
   age: {
     type: Number,
     required: [true, "Age is required"],
+    min: 1,
+    max: 100,
   },
   description: {
     type: String,
     required: [true, "Description is required"],
+    minLength: 5,
+    maxLength: 50,
   },
   location: {
     type: String,
     required: [true, "Location is required"],
+    minLength: 5,
+    maxLength: 50,
   },
   owner: {
     type: Types.ObjectId,
--- src/models/User.js
+++ src/models/User.js
@@ -5,14 +5,18 @@ const userSchema = new Schema({
   username: {
     type: String,
     required: [true, "Email is required"],
+    unique: true,
+    minLength: 2
   },
   email: {
     type: String,
     required: [true, "Username is required"],
+    minLength: 10,
   },
   password: {
     type: String,
     required: [true, "Password is required"],
+    minLength: 10,
   },
 });
 
